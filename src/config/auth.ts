1efdd9963b2159a02289b9724af283a194f20350
Victor Ribeiro <jvictorjy@gmail.com>
2020-04-29 20:07:18 -0300

    Initial commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '4cb083ca9e9101a0d8b84739eb215e24',
+    expiresIn: '1d',
+  },
+};
---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+const appointmentsRoutes = Router();
+
+appointmentsRoutes.use(ensureAuthenticated);
+
+appointmentsRoutes.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
+
+  return response.json(appointments);
+});
+
+appointmentsRoutes.post('/', async (request, response) => {
+  try {
+    const { provider_id, date } = request.body;
+
+    const parseDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService();
+
+    const appointment = await createAppointment.execute({
+      provider_id,
+      date: parseDate,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default appointmentsRoutes;
