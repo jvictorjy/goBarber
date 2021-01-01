8f5fe37a13d41b351d8aa8ed81bd617c57863a32
Victor Ribeiro <jvictorjy@gmail.com>
2021-01-01 13:49:42 -0300

    Cache with Redis

--- src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -1,14 +1,18 @@
 import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
 import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import FakeCacheProvider from '@shared/container/providers/CacheProvider/fakes/FakeCacheProvider';
 
 let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let fakeCacheProvider: FakeCacheProvider;
 let listProviderAppointments: ListProviderAppointmentsService;
 
 describe('ListProviderAppointments', () => {
   beforeEach(() => {
     fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    fakeCacheProvider = new FakeCacheProvider();
     listProviderAppointments = new ListProviderAppointmentsService(
       fakeAppointmentsRepository,
+      fakeCacheProvider,
     );
   });
 
